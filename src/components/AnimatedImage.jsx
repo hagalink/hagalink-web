5b721f8790863d597a3e8ecb0c90adae0f76a578
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-03-29 16:08:38 +0100

    Agrega nuevos componentes Servicios y SliderStackTecnico; actualiza el componente AnimatedImage y modifica el layout principal para incluir secciones de servicios y stack técnico.

--- src/components/AnimatedImage.jsx
+++ src/components/AnimatedImage.jsx
@@ -2,13 +2,13 @@ import { motion } from "framer-motion";
 
 const AnimatedImage = () => {
   return (
-    <div className="h-screen w-screen flex gap-20 items-center justify-center text-center relative overflow-hidden">
+    <div className="h-screen w-screen flex flex-col items-center justify-center text-center relative overflow-hidden">
       <motion.img
         src="/favicon.svg" 
         alt="AnimatedImage Image"
         className="absolute"
         initial={{ scale: 0.5, opacity: 0 }}
-        animate={{ scale: 1.2, opacity: 1 }}
+        animate={{ scale: 1, opacity: 1 , rotate: 360 }}
         transition={{ duration: 2, ease: "easeOut" }}
       />
       <motion.div
---
+++ src/components/SliderStackTecnico.jsx
@@ -0,0 +1,34 @@
+import { InfiniteSlider } from '../components/motion-primitives/infinite-slider';
+import { SiAstro } from "react-icons/si";
+import { FaPython } from "react-icons/fa";
+import { SiDjango } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { FaJava } from "react-icons/fa";
+import { SiSpring } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { FaDocker } from "react-icons/fa";
+import { GrMysql } from "react-icons/gr";
+import { SiTypescript } from "react-icons/si";
+import { SiPostgresql } from "react-icons/si";
+
+export function SliderStackTecnico() {
+  const iconColor = "#FFFFFF";
+
+  return (
+    <InfiniteSlider gap={80} speed={80} reverse>
+      <SiAstro className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <FaPython className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <SiDjango className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <FaReact className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <RiTailwindCssFill className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <FaJava className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <SiSpring className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <SiMongodb className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <FaDocker className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <GrMysql className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <SiTypescript className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+      <SiPostgresql className="text-white w-16 h-16 sm:w-24 sm:h-24" />
+    </InfiniteSlider>
+  );
+}
