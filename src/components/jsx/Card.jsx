78a270bbfea941e1b3ddedf286fdf1fa382f09de
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-03-31 00:11:46 +0200

    ``` Actualiza el layout y estilos globales; agrega nuevos componentes y mejora la presentación de imágenes. ```

--- src/components/jsx/Card.jsx
+++ src/components/jsx/Card.jsx
@@ -1,10 +1,15 @@
-const Card = () => {
+import React from "react";
+
+const Card = ({ icon: Icon, text, title }) => {
   return (
-    <div className="card">
-      <h2>Card Title</h2>
-      <p>This is a simple card component.</p>
+    <div className="relative w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg bg-gradient-to-b from-[#141f2b] to-[#192635] text-white flex flex-col items-center p-4 sm:p-6 rounded-lg shadow-[0_4px_15px_rgba(255,255,255,0.2)] lg:shadow-none lg:transition-all lg:duration-700 lg:hover:shadow-[0_4px_15px_rgba(255,255,255,0.2)]">
+      <div className="text-white text-3xl sm:text-4xl mb-4">
+        <Icon className="w-10 h-10 sm:w-12 sm:h-12" />
+      </div>
+      <h3 className="font-bold text-base sm:text-lg md:text-xl text-center mb-2 glow-title">{title}</h3>
+      <p className="text-sm sm:text-base text-gray-300 text-center">{text}</p>
     </div>
   );
-}
+};
 
 export default Card;
