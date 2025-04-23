9f4ee237896c29ffbffd0813d9dd862658cf0cd2
alvaroofernaandez <alvaroofernaandez@gmail.com>
2025-04-23 12:56:42 +0200

    Mejora la clase de transición en el componente Team y ajusta el estilo del contenedor de información de los miembros del equipo.

--- src/components/jsx/Team.jsx
+++ src/components/jsx/Team.jsx
@@ -41,19 +41,19 @@ const Team = () => {
           target="_blank"
           rel="noopener noreferrer"
           className="relative text-center group rounded-2xl overflow-hidden 
-            md:transition-transform md:duration-1000 lg:hover:scale-110"
+            transition-transform duration-1000 lg:hover:scale-110"
         >
           <img
             src={member.img}
             alt={member.name}
-            className="w-full h-auto max-w-[250px] max-h-[400px] object-cover img-shadow img-gradient filter grayscale hover:grayscale-0"
+            className="w-full h-auto max-w-[250px] max-h-[400px] object-cover img-shadow img-gradient filter grayscale hover:grayscale-0 rounded-2xl"
             loading="lazy"
           />
           
           <div
             className="absolute bottom-0 left-0 w-full bg-black/70
               flex flex-col items-center justify-center text-white space-y-1 py-4 
-              opacity-100 lg:opacity-0 md:group-hover:opacity-100 transition-opacity duration-700"
+              opacity-100 lg:opacity-0 group-hover:opacity-100 transition-opacity duration-700 rounded-b-2xl"
           >
             <h7 className="text-md font-semibold ">{member.name}</h7>
             <p className="text-sm font-extralight">{member.title}</p>
